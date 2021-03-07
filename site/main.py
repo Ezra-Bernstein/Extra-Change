9cbb0dc5d7b443a9f9ea73990f2fc0e5ac4294f9
Ezra Bernstein <mremdb@gmail.com>
2021-03-07 03:12:29 -0500

    Update main.py
    Fixed some links to templates, other pages
    Gets proper data on post request to create new fund
    Allows access to any fund with correct url
    Added functionality for adding funds from extension

--- site/main.py
+++ site/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from dotenv import load_dotenv
 import os
-from datastax_functions import createUser, getUserID, getUserData, checkUser, userExists, createFund, addToFund
+from datastax_functions import createUser, getUserID, getUserData, checkUser, userExists, createFund, addToFund, getFundsList, getFund
 
 load_dotenv()
 
@@ -29,7 +29,7 @@ def signup():
         else:
             createUser(username, password)
             session['username'] = username
-            return redirect(url_for('newFund'))
+            return redirect(url_for('dashboard'))
     elif request.method=='GET':
         return render_template('signup.html')
 
@@ -46,47 +46,62 @@ def login():
         
         if checkUser(username, password):
             session['username'] = username
-            return redirect(url_for('newFund'))
+            return redirect(url_for('dashboard'))
         else:
            return redirect(url_for('login'))
 
 
     elif request.method=='GET':
-        return render_template('login.html')
+        return render_template('landing.html')
     
 @app.route('/logout')
 def logout():
     session.pop('username')
     return redirect(url_for('home'))
 
+@app.route('/dashboard')
+def dashboard():
+    data = getUserData(getUserID(session['username']))
+    return render_template('dashboard.html', data=data)
+
+
 @app.route('/newFund', methods=['GET', 'POST'])
 def newFund():
     if request.method=='GET':
-        return render_template('dashboard.html')
+        return render_template('createfund.html')
 
     elif request.method=='POST':
-        fundName = request.form['fundName']
-        fundDesc = request.form['fundDesc']
-        fundGoal = request.form['fundGoal']
-        fundCreator = session['user']
-        createFund(fundName, fundDesc, fundGoal)
+        fundName = request.form['name-fund']
+        fundDesc = request.form['description']
+        fundGoal = request.form['goal']
+        fundCreator = session['username']
+        createFund(fundName, fundDesc, fundGoal, fundCreator)
         return redirect(url_for('fund'))
 
-@app.route('/fund')
-def fund():
-    fundData = getUserData(session['username'])
-    return render_template('fund.html', fundData)
+@app.route('/fund/<username>/<fundName>')
+def fund(username, fundName):
+    fundData = getFund(username, fundName)
+    return render_template('view.html', data = fundData)
 
 @app.route('/addFunds', methods = ['POST'])
 def addFunds():
+    select = request.form.get('selectedFund')
+    print(select)
+
     amount = request.form['amount']
-    #creator = ??
-    fundName = request.form['fundName']
+    print(amount)
 
-    return redirect(url_for('fund'))
+    fundData = select.split(',')
+    fundName = fundData[0]
+    creator = fundData[1]
+    print(fundName)
+    print(creator)
+    #addToFund(fundName, creator, amount)
+    return 'Funds Added!'
 
-
-
+@app.route('/getFundList', methods = ['GET'])
+def getFundList():
+    return getFundsList()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
